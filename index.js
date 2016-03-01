4b97e6c6e329350a9b411a03f1184d956fa86f69
DuraNn <durann2@gmail.com>
2016-03-01 16:58:25 +0100

    v2.0.0 - Added Warning Messages

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
   var _ = '\x1b[';
   var COLOR = {
       RED: _ + '91m',
+      YELLOW: _ + '93m',
       LIGHT_RED: _ + '31m',
       DARK_BLUE: _ + '34m',
       BLUE: _ + '94m',
@@ -17,8 +18,12 @@
   };
 
   var ESLintReporter = function(loggerFactory, eslintPreprocessorConfig) {
+      console.log("=============================================");
+      console.log(eslintPreprocessorConfig);
+      console.log("=============================================");
     var log = loggerFactory.create('preprocessor.eslint');
     var options = {
+      showWarnings: getOptionWithFallback('showWarnings', true),
       stopOnError: getOptionWithFallback('stopOnError', true),
       stopOnWarning: getOptionWithFallback('stopOnWarning', false)
     };
@@ -39,26 +44,49 @@
       };
 
       results.forEach(function(result) {
-        if(result.errorCount === 1) {
-          log.error('\n' +
-            COLOR.LIGHT_RED + result.filePath + '\n' + COLOR.RESET +
-            getError(result.messages[0]) + '\n\n'
-          );
-        } else if(result.errorCount > 0) {
+	  if(result.errorCount > 0) {
           var errors = [];
           result.messages.forEach(function(message) {
-            errors.push(getError(message));
+	      if(message.severity > 1){
+		  errors.push(getError(message));
+	      }
           });
           log.error('\n' +
-            COLOR.LIGHT_RED + result.errorCount + ' errors in ' + result.filePath + '\n' + COLOR.RESET +
+            COLOR.LIGHT_RED + result.errorCount + ' error(s) in ' + result.filePath + '\n' + COLOR.RESET +
             errors.join('\n') + '\n\n'
           );
         }
       });
     }
 
+    function processWarnings(results) {
+      var getWarning = function(message) {
+        return '   - ' + COLOR.YELLOW + message.line +
+          ':' + message.column + COLOR.RESET + ' ' +
+          COLOR.DARK_BLUE + message.message + COLOR.RESET;
+      };
+
+      results.forEach(function(result) {
+	  if(result.warningCount > 0) {
+          var warnings = [];
+          result.messages.forEach(function(message) {
+	      if(message.severity === 1){
+		  warnings.push(getWarning(message));
+	      }
+          });
+          log.warn('\n' +
+            COLOR.YELLOW + result.warningCount + ' warning(s) in ' + result.filePath + '\n' + COLOR.RESET +
+            warnings.join('\n') + '\n\n'
+          );
+        }
+      });
+    }
+
     function shouldStop(report) {
-      if(report.errorCount || report.warningCount) processErrors(report.results);
+      if(report.warningCount && 
+	 (options.showWarnings || options.stopOnWarning)) 
+	  processWarnings(report.results);
+      if(report.errorCount) processErrors(report.results);
       return (report.errorCount && options.stopOnError) ||
         (report.warningCount && options.stopOnWarning);
     }
